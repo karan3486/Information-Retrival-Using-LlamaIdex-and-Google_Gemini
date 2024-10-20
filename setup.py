1141d6e3fe279f348d847941321d019ae04fc825
Karan shrestha <65931700+karan3486@users.noreply.github.com>
2024-10-20 15:25:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'QApplication',
     version= '0.0.1',
-    author= 'sunny savita',
-    author_email= 'sunny.savita@gmail.com',
+    author= 'Karan Shrestha',
+    author_email= 'ks.karanshrestha@gmail.com',
     packages= find_packages(),
     install_requires = []
 
